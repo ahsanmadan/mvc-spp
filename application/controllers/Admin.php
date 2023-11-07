0c0c2713792c074190bf2dbb7d13f76e6b86a8ca
Ahsan Ramadan <ahsanramadan07@gmail.com>
2023-11-07 13:15:38 +0700

    salting abiez

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,6 +1,17 @@
 <?php
 class Admin extends CI_Controller {
 
+	public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+        $this->load->model('Admin_model', 'am'); //load model Admin
+        // cek login
+        // if(!$this->session->userdata('username')) {
+        //     redirect('Auth/forbiden');
+        // }
+    }
+	
 	public function index () {
         $this->load->view('admin/index');
 	}
---
+++ application/models/Admin_model.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin_model extends CI_Model {
+    public function tambahSiswa(){
+        // get data yang dikirim
+        $nisn   = $this->input->post('nisn');
+        $nama   = $this->input->post('nama_siswa');
+        $thn_masuk    = $this->input->post('thn_masuk');
+        $gender = $this->input->post('gender');
+        $alamat = $this->input->post('alamat');
+        $no_hp  = $this->input->post('no_hp');
+
+        // satukan semua kedalam array data
+        $data = [
+            'nisn'          => $nisn,
+            'nama_siswa'    => $nama,
+            'tgl_masuk'       => $thn_masuk,
+            'gender' => $gender,
+            'alamat'        => $alamat,
+            'no_hp'         => $no_hp
+        ];
+
+        // eksekusi query insert data ke table
+        $this->db->insert('siswa', $data);
+    }
+}
