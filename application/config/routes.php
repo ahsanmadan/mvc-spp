f611c5c6f9bc39238521feda7c35e807896957c6
Ahsan Ramadan <ahsanramadan07@gmail.com>
2024-03-05 09:54:11 +0700

    new

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 $route['default_controller'] = 'Home';
-$route['404_override'] = '';
+$route['404_override'] = 'eror/404.html';
 $route['translate_uri_dashes'] = FALSE;
